a050a0956b925f83b0a3aea586cc568977ac8e01
sbk283 <sbkcoding@gmail.com>
2025-06-16 09:37:01 +0900

    6/16 수업시작

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-window.addEventListener("DOMContentLoaded", function() {
-    // main
-    const main = this.document.querySelector(".main");
-    // 윈도우 스크롤 체크
-    window.addEventListener("scroll", function() {
-        const scY = window.scrollY;
-
-        if (scY > 0) {
-            console.log("스크롤됨");
-            main.classList.add("main_scroll");
-        } else {
-            console.log("스크롤 최상단");
-            main.classList.remove("main_scroll");
-        }
-    });
+window.addEventListener("DOMContentLoaded", function () {
+  // main
+  const main = this.document.querySelector(".main");
+  // 윈도우 스크롤 체크
+  window.addEventListener("scroll", function () {
+    const scY = window.scrollY;
+    if (scY > 0) {
+      main.classList.add("main_scroll");
+    } else {
+      main.classList.remove("main_scroll");
+    }
+  });
 });
---
+++ js/ticket.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+  // swiper 만들기 실행
+  new Swiper(".sw_ticket", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    navigation: {
+      nextEl: ".ticket_slide_next",
+      prevEl: ".ticket_slide_prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
